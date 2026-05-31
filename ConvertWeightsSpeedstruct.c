47c8636c548cac126459123d91f2f1ef6a474325
Mitchell Wise <86785448+mwiseca@users.noreply.github.com>
2026-05-31 08:58:11 -0400

    Update copyright year in license information

--- ConvertWeightsSpeedstruct.c
+++ ConvertWeightsSpeedstruct.c
@@ -1,6 +1,6 @@
 /*MIT License
 
-  Copyright (c) 2021-2025 Mitchell E Wise
+  Copyright (c) 2021-2026 Mitchell E Wise
 
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
@@ -620,7 +620,7 @@ void lightYearsToMilesBasic() {
 void license() {
     printf("\n MIT License\n\n");
 
-    printf(" Copyright (c) 2021-2025 Mitchell E Wise\n\n");
+    printf(" Copyright (c) 2021-2026 Mitchell E Wise\n\n");
 
     printf(" Permission is hereby granted, free of charge, to any person obtaining a copy\n");
     printf(" of this software and associated documentation files (the \"Software\"), to deal\n");
